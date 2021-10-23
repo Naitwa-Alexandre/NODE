c54beb1b1005778aed8b19fd52328190d7627ac9
Ale Pereira <Naitwa-Alexandre@github.com>
2021-10-23 16:47:28 -0300

    fs async

---
+++ node_tutorial/11-fs-async.js
@@ -0,0 +1,19 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt','utf8', (err, res) => {
+  if (err) return console.log(err);
+  const first = res;
+  readFile('./content/second.txt', 'utf8', (err, res) => {
+    if (err) return console.log(err);
+    const second = res;
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result: ${ first }, ${ second }`,
+      { flag: 'a' },
+      (err, res) => {
+        if (err) return console.log(err);
+        console.log('file created');
+      }
+    );
+  });
+});
