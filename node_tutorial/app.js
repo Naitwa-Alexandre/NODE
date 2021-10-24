647450ccb2b6d44fb12d47ff2db0ed5f433203d3
Ale Pereira <Naitwa-Alexandre@github.com>
2021-10-23 22:17:04 -0300

    first package

--- node_tutorial/app.js
+++ node_tutorial/app.js
@@ -1,23 +1,7 @@
-const http = require('http');
+const _ = require('lodash');
 
-const server = http.createServer((req, res) => {
-  const { url } = req;
-  switch (url) {
-    case '/':
-      res.end('Welcome to our home page');
-      break;
-    case '/about':
-      res.end('Welcome to about page');
-    case '/sports':
-      res.end('Welcome to sports page');
-    default:
-      break;
-  }
-  res.end(`
-    <h1>Ooops!!</h1>
-    <p>We could not found the page you are looking for</p>
-    <a href="/">back home</a>
-  `);
-});
+const items = ['1', ['2', ['3'], [5, [10]]]];
 
-server.listen(5000);
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
